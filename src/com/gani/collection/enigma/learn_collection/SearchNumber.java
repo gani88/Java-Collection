c6863add74adb8cbcd66e15bc12d83063abb6e4e
zarekgema8 <zarekgema8@gmail.com>
2024-05-01 07:57:01 +0700

    add search number file

--- src/com/gani/collection/enigma/learn_collection/SearchNumber.java
+++ src/com/gani/collection/enigma/learn_collection/SearchNumber.java
@@ -5,8 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-
-
 public class SearchNumber {
     public static void main(String[] args) {
 //        System.out.println("Please input numbers : ");
@@ -20,19 +18,55 @@ public class SearchNumber {
 //        input.close();
 
         // BIG & SMALL
-        List<Integer> numbers = List.of(1, 4, 5, 6, 7, 6, -100, 50, 5, -10, 100, 67, -99);
-        Integer tempBig = numbers.get(0);
-        Integer tempSmall = numbers.get(0);
+//        List<Integer> numbers = List.of(1, 4, 5, 6, 7, 6, -100, 50, 5, -10, 100, 67, -99);
+//        Integer tempBig = numbers.get(0);
+//        Integer tempSmall = numbers.get(0);
+//
+//        for (int i = 0; i < numbers.size(); i++) {
+//            if (numbers.get(i) > tempBig) {
+//                tempBig = numbers.get(i);
+//            }else if (numbers.get(i) < tempSmall) {
+//                tempSmall = numbers.get(i);
+//            }
+//        }
+//        System.out.println("Angka Terbesar : " + tempBig);
+//        System.out.println("Angka Terkecil : " + tempSmall);
 
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) > tempBig) {
-                tempBig = numbers.get(i);
-            }else if (numbers.get(i) < tempSmall) {
-                tempSmall = numbers.get(i);
+        // Improved Code
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Please input numbers (type any non-integer to stop): ");
+//        List<Integer> numbers = new ArrayList<>();
+//        while (input.hasNextInt()) {
+//            numbers.add(input.nextInt());
+//        }
+//        input.close();
+
+        List<Integer> numbers = new ArrayList<>();
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Please input numbers (type any non-integer to stop): ");
+            if (!input.hasNextInt()) {
+                break;
+            } else {
+                numbers.add(input.nextInt());
             }
         }
-        System.out.println("Angka Terbesar : " + tempBig);
-        System.out.println("Angka Terkecil : " + tempSmall);
+
+        Integer largest = numbers.get(0);
+        Integer smallest = numbers.get(0);
+
+        for (Integer num : numbers) {
+            if (num > largest) {
+                largest = num;
+            }
+            if (num < smallest) {
+                smallest = num;
+            }
+        }
+
+        System.out.println("Largest number: " + largest);
+        System.out.println("Smallest number: " + smallest);
 
         System.out.println("===============Using method================");
         System.out.println("MAX : " + Collections.max(numbers));
